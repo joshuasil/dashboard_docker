b593e9fe905e9f278f8795e7c92aea05a40225dd
joshva2104 <49963261+joshva2104@users.noreply.github.com>
2022-07-11 10:09:04 -0600

    revert back to reg plot

--- project/app/app.py
+++ project/app/app.py
@@ -1,9 +1,24 @@
 import dash
 import dash_bootstrap_components as dbc
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import dcc
+from dash import html
 from dash.dependencies import Input, Output
 from functions import plot_regression
+
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+
+# app requires "pip install psycopg2" as well
+
+server = Flask(__name__)
+app = dash.Dash(__name__, server=server, suppress_callback_exceptions=True)
+app.server.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# for your home PostgreSQL test table
+# app.server.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:your_password@localhost/test"
+
+# for your live Heroku PostgreSQL database
+app.server.config["SQLALCHEMY_DATABASE_URI"] = "postgres://joshva:Muscreen1!@ec2-54-70-152-149.us-west-2.compute.amazonaws.com:5432/chatbot_log"
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 server = app.server
@@ -36,3 +51,6 @@ app.layout = html.Div(
 )
 def update_regression_plot(std):
     return plot_regression(std)
+
+if __name__ == '__main__':
+    app.run_server(host="0.0.0.0",port=8050,debug=True)
